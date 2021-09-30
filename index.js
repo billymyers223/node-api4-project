02156da1d2f3040ff5a5569e475ecb6b6556ee79
billmyers223 <billymyers223@gmail.com>
2021-09-30 15:02:28 -0500

    setting up

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-console.log('Web 46 rules')
+
 require('dotenv').config()
 const express = require('express')
 const server = express()
 server.use(express.json())
 
 
+server.get('/',  (req,res) =>{
+    res.json({message:'Web46 rocks!'})
+})
 server.get('/hello', (req,res) =>{
     res.send('<h1>HELLO WORLD</h1>')
 }) 
